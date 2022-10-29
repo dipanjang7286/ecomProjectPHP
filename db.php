e533fe597895569faaa359230825c9681529e15c
dipanjang7286 <71178502+dipanjang7286@users.noreply.github.com>
2022-10-29 14:17:03 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "himalayan";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, "project");
