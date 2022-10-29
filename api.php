67dbf2ae6ba2d3628b7ce13b532f3da749ff52c7
dipanjang7286 <71178502+dipanjang7286@users.noreply.github.com>
2022-10-29 14:18:27 +0530

    Update api.php

--- api.php
+++ api.php
@@ -113,7 +113,7 @@ if (isset($_POST['update_user'])) {
 }
 
 ////// delete User
-$con = mysqli_connect("localhost", "root", "himalayan", "project");
+$con = mysqli_connect("localhost", "root", "", "project");
 if (isset($_POST['delete_user'])) {
     $user_id = mysqli_real_escape_string($con, $_POST['user_id']);
     $delete_query = "DELETE FROM users WHERE id='$user_id'"; // Deleting data
